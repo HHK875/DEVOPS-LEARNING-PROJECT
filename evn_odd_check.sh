b192565f758fdacba03f655b4e8dcbd741352275
harish <harishahk14@gmail.com>
2022-08-02 11:36:39 +0530

    Inserted new lines as needed to improve the readability

--- evn_odd_check.sh
+++ evn_odd_check.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo $0 # To print the script name
+
 #set -x # Debugging
 echo " Enter the number to check whether it is even or odd "
 read NUM
+
 RESULT=` expr $NUM % 2`
+
 if [ $RESULT  -eq 0 ];
 then
 	echo " Entered number is even "
